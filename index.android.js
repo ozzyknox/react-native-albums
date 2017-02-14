c987a57f06596551e7b003f2e3ffbafab7ec126e
ozzyawaken <ozzy.knox@awakensoftware.com>
2017-02-14 11:58:29 +0100

    first commit

---
+++ index.android.js
@@ -0,0 +1,17 @@
+// Import
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+// Create component
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+);
+
+// Render to device
+AppRegistry.registerComponent('albums', () => App);
